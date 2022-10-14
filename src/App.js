287d85ef8304e6629288d26842ad931d99fc69cc
chris <chrishethertonweb@gmail.com>
2022-10-14 09:57:45 +0200

    added json file and RandomList created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import RandomList from './RandomList'
+
+
 
 export default function App() {
     return (
         <div>
-            <h1>Hello World</h1>
+            <RandomList />
         </div>
     )
 }
